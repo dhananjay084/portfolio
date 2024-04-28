c4e8c769407594c750cd8df661c5318f20a44430
unknown <dhananjaybansal28@gmail.com>
2024-04-28 15:05:41 +0530

    first commit

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -12,9 +12,11 @@ const Navbar = () => {
     <Nav>
       <NavbarContainer>
         <NavLogo to='/'>
-          <a href="#" style={{ display: "flex", alignItems: "center", color: "white", marginBottom: '20;', cursor: 'pointer' }}>
-            <DiCssdeck size="3rem" /> <Span>Portfolio</Span>
-          </a>
+          {/* <a href="#" style={{ display: "flex", alignItems: "center", color: "white", marginBottom: '20;', cursor: 'pointer' }}>
+            <DiCssdeck size="3rem" />
+             
+          </a> */}
+          <Span>Portfolio</Span>
         </NavLogo>
         <MobileIcon>
           <FaBars onClick={() => {
